58bd8b59d5580afd829f37d5a65428c68c57e9f5
Dian Nguyen <dnguyen@student.neumont.edu>
2024-11-26 20:57:46 -0700

    Fixed Collision in Build

--- Assets/Scripts/RandomizedLevel/SpawnObject.cs
+++ Assets/Scripts/RandomizedLevel/SpawnObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SpawnObject : MonoBehaviour
 {
@@ -11,5 +12,20 @@ public class SpawnObject : MonoBehaviour
         int random = Random.Range(0, objects.Length);
         GameObject instance = (GameObject)Instantiate(objects[random], transform.position, Quaternion.identity);
         instance.transform.parent = transform;
+
+        foreach (GameObject obj in objects)
+        {
+            TilemapCollider2D tilemapCollider = obj.GetComponent<TilemapCollider2D>();
+            if (tilemapCollider != null)
+            {
+                tilemapCollider.enabled = false; // Disable and re-enable to force refresh
+                tilemapCollider.enabled = true;
+            }
+
+            Physics2D.SyncTransforms(); // Sync the physics system
+
+            //Debug.Log($"Tilemap Collider Enabled: {tilemapCollider.enabled}");
+            //Debug.Log($"Tilemap Rigidbody: {obj.GetComponent<Rigidbody2D>()}");
+        }
     }
 }
